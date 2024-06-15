d11a194f4745a5df7e456fcb818e3ed53ffd8968
Qmi <minh.pham1126@gmail.com>
2024-06-15 14:48:47 +0700

    feat: implement flag struct

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
+const C_BYTE_POSITION: u8 = 0;
+const Z_BYTE_POSITION: u8 = 1;
+const I_BYTE_POSITION: u8 = 2;
+const D_BYTE_POSITION: u8 = 3;
+const B_BYTE_POSITION: u8 = 4;
+const V_BYTE_POSITION: u8 = 6;
+const N_BYTE_POSITION: u8 = 7;
+
+pub struct Flag {
+    BreakCommand: bool,
+    Carry: bool,
+    DecimalMode: bool,
+    Interrupt: bool,
+    Negative: bool,
+    Overflow: bool,
+    Zero: bool,
+}
 pub struct CPU {
     RegisterA: u8,
     RegisterX: u8,
     RegisterY: u8,
     StackPointer: u8,
     ProgramCounter: u16,
-    FlagB: bool,
-    FlagC: bool,
-    FlagD: bool,
-    FlagI: bool,
-    FlagN: bool,
-    FlagV: bool,
-    FlagZ: bool,
+    Flag: Flag,
 }
 impl CPU {
     pub fn new() -> Self {
@@ -38,7 +49,11 @@ impl CPU {
             self.ProgramCounter += 1;
 
             match opcode {
-                _ => todo!()
+                0xA9 => {
+                    let param = program[self.ProgramCounter as usize];
+                    self.ProgramCounter += 1;
+                    self.RegisterA = param;
+                }
             }
         }
     }
