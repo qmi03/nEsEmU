76b72a623d0a19244a1691850452bed6ea027cdf
Qmi <minh.pham1126@gmail.com>
2024-06-16 19:00:26 +0700

    refactor: rename for convention

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@ const N_BYTE_POSITION: u8 = 7;
 
 impl std::convert::From<Flag> for u8 {
     fn from(flag: Flag) -> u8 {
-        (if flag.Carry { 1 } else { 0 }) << C_BYTE_POSITION
-            | (if flag.Zero { 1 } else { 0 }) << Z_BYTE_POSITION
-            | (if flag.Interrupt { 1 } else { 0 }) << I_BYTE_POSITION
-            | (if flag.DecimalMode { 1 } else { 0 }) << D_BYTE_POSITION
-            | (if flag.BreakCommand { 1 } else { 0 }) << B_BYTE_POSITION
-            | (if flag.Overflow { 1 } else { 0 }) << V_BYTE_POSITION
-            | (if flag.Negative { 1 } else { 0 }) << N_BYTE_POSITION
+        (if flag.carry { 1 } else { 0 }) << C_BYTE_POSITION
+            | (if flag.zero { 1 } else { 0 }) << Z_BYTE_POSITION
+            | (if flag.interrupt { 1 } else { 0 }) << I_BYTE_POSITION
+            | (if flag.decimal_mode { 1 } else { 0 }) << D_BYTE_POSITION
+            | (if flag.break_command { 1 } else { 0 }) << B_BYTE_POSITION
+            | (if flag.overflow { 1 } else { 0 }) << V_BYTE_POSITION
+            | (if flag.negative { 1 } else { 0 }) << N_BYTE_POSITION
     }
 }
 
@@ -34,69 +34,69 @@ impl std::convert::From<u8> for Flag {
         let negative = ((value >> N_BYTE_POSITION) & 0b1) != 0;
 
         Flag {
-            BreakCommand: break_command,
-            Carry: carry,
-            DecimalMode: decimal_mode,
-            Interrupt: interrupt,
-            Negative: negative,
-            Overflow: overflow,
-            Zero: zero,
+            break_command,
+            carry,
+            decimal_mode,
+            interrupt,
+            negative,
+            overflow,
+            zero,
         }
     }
 }
 pub struct Flag {
-    BreakCommand: bool,
-    Carry: bool,
-    DecimalMode: bool,
-    Interrupt: bool,
-    Negative: bool,
-    Overflow: bool,
-    Zero: bool,
+    break_command: bool,
+    carry: bool,
+    decimal_mode: bool,
+    interrupt: bool,
+    negative: bool,
+    overflow: bool,
+    zero: bool,
 }
 impl Flag {
     pub fn reset(&mut self) {
-        self.BreakCommand = false;
-        self.Carry = false;
-        self.DecimalMode = false;
-        self.Interrupt = false;
-        self.Negative = false;
-        self.Overflow = false;
-        self.Zero = false;
+        self.break_command = false;
+        self.carry = false;
+        self.decimal_mode = false;
+        self.interrupt = false;
+        self.negative = false;
+        self.overflow = false;
+        self.zero = false;
     }
 }
-pub struct CPU {
-    RegisterA: u8,
-    RegisterX: u8,
-    RegisterY: u8,
-    StackPointer: u8,
-    ProgramCounter: u16,
-    Flag: Flag,
-    Memory: [u8; 0xffff],
+pub struct Cpu {
+    register_a: u8,
+    register_x: u8,
+    register_y: u8,
+    stack_pointer: u8,
+    program_counter: u16,
+    flag: Flag,
+    memory: [u8; 0xffff],
 }
-impl CPU {
+impl Cpu {
     pub fn new() -> Self {
-        CPU {
-            RegisterA: 0,
-            RegisterX: 0,
-            RegisterY: 0,
-            StackPointer: 0,
-            ProgramCounter: 0,
-            Flag: Flag::from(0),
-            Memory: [0; 0xffff],
+        Cpu {
+            register_a: 0,
+            register_x: 0,
+            register_y: 0,
+            stack_pointer: 0,
+            program_counter: 0,
+            flag: Flag::from(0),
+            memory: [0; 0xffff],
         }
     }
     pub fn reset(&mut self) {
-        self.RegisterA = 0;
-        self.RegisterX = 0;
-        self.RegisterY = 0;
-        self.Flag.reset();
-        self.ProgramCounter = self.read_mem_16(0xFFFC);
+        self.register_a = 0;
+        self.register_x = 0;
+        self.register_y = 0;
+        self.flag.reset();
+        self.program_counter = self.read_mem_16(0xFFFC);
     }
     fn read_mem(&self, address: u16) -> u8 {
-        self.Memory[address as usize]
+        self.memory[address as usize]
     }
     fn write_mem(&mut self, address: u16, data: u8) {
-        self.Memory[address as usize] = data;
+        self.memory[address as usize] = data;
     }
     fn read_mem_16(&self, address: u16) -> u16 {
         let least_significant_byte = self.read_mem(address) as u16;
@@ -116,23 +116,23 @@ impl CPU {
         self.run();
     }
     fn load(&mut self, program: Vec<u8>) {
-        self.Memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
         self.write_mem_16(0xFFFC, 0x8000);
     }
     fn run(&mut self) {
         loop {
-            let opcode = self.read_mem(self.ProgramCounter);
-            self.ProgramCounter += 1;
+            let opcode = self.read_mem(self.program_counter);
+            self.program_counter += 1;
 
             match opcode {
                 0xA9 => {
-                    let param = self.read_mem(self.ProgramCounter);
-                    self.ProgramCounter += 1;
+                    let param = self.read_mem(self.program_counter);
+                    self.program_counter += 1;
                     self.lda(param);
                 }
                 0xA2 => {
-                    let param = self.read_mem(self.ProgramCounter);
-                    self.ProgramCounter += 1;
+                    let param = self.read_mem(self.program_counter);
+                    self.program_counter += 1;
                     self.ldx(param);
                 }
                 0xAA => self.tax(),
@@ -143,26 +143,26 @@ impl CPU {
         }
     }
     fn lda(&mut self, value: u8) {
-        self.RegisterA = value;
-        self.update_zero_and_negative_flag(self.RegisterA);
+        self.register_a = value;
+        self.update_zero_and_negative_flag(self.register_a);
     }
     fn ldx(&mut self, value: u8) {
         println!("WENT INTO LDX FUNCTION{}", value);
-        self.RegisterX = value;
-        self.update_zero_and_negative_flag(self.RegisterX);
+        self.register_x = value;
+        self.update_zero_and_negative_flag(self.register_x);
     }
     fn tax(&mut self) {
-        self.RegisterX = self.RegisterA;
-        self.update_zero_and_negative_flag(self.RegisterX);
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flag(self.register_x);
     }
     fn update_zero_and_negative_flag(&mut self, result: u8) {
-        self.Flag.Zero = result == 0;
-        self.Flag.Negative = result & 0b1000_0000 != 0
+        self.flag.zero = result == 0;
+        self.flag.negative = result & 0b1000_0000 != 0
     }
 
     fn inx(&mut self) {
-        self.RegisterX = self.RegisterX.wrapping_add(1);
-        self.update_zero_and_negative_flag(self.RegisterX)
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flag(self.register_x)
     }
 }
 
@@ -172,80 +172,80 @@ mod test {
 
     #[test]
     fn test_0xa9_lda_immediate_load_data() {
-        let mut cpu = CPU::new();
+        let mut cpu = Cpu::new();
         cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
-        assert_eq!(cpu.RegisterA, 0x05);
-        assert!(cpu.Flag.Zero == false);
-        assert!(cpu.Flag.Negative == false);
+        assert_eq!(cpu.register_a, 0x05);
+        assert!(cpu.flag.zero == false);
+        assert!(cpu.flag.negative == false);
     }
 
     #[test]
     fn test_0xa9_lda_zero_flag() {
-        let mut cpu = CPU::new();
+        let mut cpu = Cpu::new();
         cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
-        assert!(cpu.Flag.Zero == true);
+        assert!(cpu.flag.zero == true);
     }
 
     #[test]
     fn test_0xa2_ldx_immediate_load_data() {
-        let mut cpu = CPU::new();
+        let mut cpu = Cpu::new();
         cpu.load_and_run(vec![0xa2, 0x05, 0x00]);
-        assert_eq!(cpu.RegisterX, 0x05);
-        assert!(cpu.Flag.Zero == false);
-        assert!(cpu.Flag.Negative == false);
+        assert_eq!(cpu.register_x, 0x05);
+        assert!(cpu.flag.zero == false);
+        assert!(cpu.flag.negative == false);
     }
 
     #[test]
     fn test_0xa2_ldx_zero_flag() {
-        let mut cpu = CPU::new();
+        let mut cpu = Cpu::new();
         cpu.load_and_run(vec![0xa2, 0x00, 0x00]);
-        assert!(cpu.Flag.Zero == true);
+        assert!(cpu.flag.zero == true);
     }
 
     #[test]
     fn test_0xa9_lda_negative_flag() {
-        let mut cpu = CPU::new();
+        let mut cpu = Cpu::new();
         cpu.load_and_run(vec![0xa9, 0xf1, 0x00]);
-        assert!(cpu.Flag.Negative == true);
+        assert!(cpu.flag.negative == true);
     }
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
-        let mut cpu = CPU::new();
-        cpu.RegisterA = 10;
+        let mut cpu = Cpu::new();
+        cpu.register_a = 10;
         cpu.load_and_run(vec![0xa9, 10, 0xaa, 0x00]);
 
-        assert_eq!(cpu.RegisterX, 10)
+        assert_eq!(cpu.register_x, 10)
     }
     #[test]
     fn test_0xaa_tax_move_a_to_x_zero_flag() {
-        let mut cpu = CPU::new();
-        cpu.RegisterA = 0;
+        let mut cpu = Cpu::new();
+        cpu.register_a = 0;
         cpu.load_and_run(vec![0xa9, 0, 0xaa, 0x00]);
 
-        assert_eq!(cpu.Flag.Zero, true)
+        assert_eq!(cpu.flag.zero, true)
     }
     #[test]
     fn test_0xaa_tax_move_a_to_x_negative_flag() {
-        let mut cpu = CPU::new();
+        let mut cpu = Cpu::new();
         cpu.load_and_run(vec![0xa9, 0xf1, 0xaa, 0x00]);
 
-        assert_eq!(cpu.Flag.Negative, true)
+        assert_eq!(cpu.flag.negative, true)
     }
     #[test]
     fn test_5_ops_working_together() {
-        let mut cpu = CPU::new();
+        let mut cpu = Cpu::new();
         cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
 
-        assert_eq!(cpu.RegisterX, 0xc1)
+        assert_eq!(cpu.register_x, 0xc1)
     }
 
     #[test]
     fn test_inx_overflow() {
-        let mut cpu = CPU::new();
-        cpu.RegisterX = 0xff;
+        let mut cpu = Cpu::new();
+        cpu.register_x = 0xff;
         cpu.load_and_run(vec![0xa2, 0xff, 0xe8, 0xe8, 0x00]);
 
-        assert_eq!(cpu.RegisterX, 1)
+        assert_eq!(cpu.register_x, 1)
     }
 }
 fn main() {
